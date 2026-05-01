3dfccf57dad7e27d94c2c1af9797bfb907f5af63
Jazzdar1 <darajazb@gmail.com>
2026-05-01 14:43:25 +0530

    Update tabs.tsx

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -11,7 +11,10 @@ const TabsList = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
-    className={cn('inline-flex h-10 items-center justify-center rounded-md bg-white/10 p-1 text-white', className)}
+    className={cn(
+      'inline-flex w-full items-center rounded-xl bg-white/10 p-1 text-white overflow-x-auto no-scrollbar',
+      className
+    )}
     {...props}
   />
 ));
@@ -24,7 +27,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Tab
     ref={ref}
     className={cn(
-      'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium transition-all data-[selected]:bg-black/50 data-[selected]:text-white',
+      'inline-flex min-h-9 flex-1 shrink-0 items-center justify-center whitespace-nowrap rounded-lg px-3 py-2 text-xs sm:text-sm font-semibold transition-all data-[selected]:bg-black/50 data-[selected]:text-white',
       className
     )}
     {...props}
@@ -36,7 +39,7 @@ const TabsContent = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Panel>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.Panel>
 >(({ className, ...props }, ref) => (
-  <TabsPrimitive.Panel ref={ref} className={cn('mt-2', className)} {...props} />
+  <TabsPrimitive.Panel ref={ref} className={cn('mt-3 sm:mt-4', className)} {...props} />
 ));
 TabsContent.displayName = 'TabsContent';
 
