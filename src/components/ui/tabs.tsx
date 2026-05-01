31c077893f854260896a108ca3547bad1a561e63
Jazzdar1 <darajazb@gmail.com>
2026-05-01 14:21:50 +0530

    Add Tabs component with List, Trigger, and Content

---
+++ src/components/ui/tabs.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { Tabs as TabsPrimitive } from '@base-ui/react/tabs';
+
+import { cn } from '@/lib/utils';
+
+const Tabs = TabsPrimitive.Root;
+
+const TabsList = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.List
+    ref={ref}
+    className={cn('inline-flex h-10 items-center justify-center rounded-md bg-white/10 p-1 text-white', className)}
+    {...props}
+  />
+));
+TabsList.displayName = 'TabsList';
+
+const TabsTrigger = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Tab>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Tab>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.Tab
+    ref={ref}
+    className={cn(
+      'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium transition-all data-[selected]:bg-black/50 data-[selected]:text-white',
+      className
+    )}
+    {...props}
+  />
+));
+TabsTrigger.displayName = 'TabsTrigger';
+
+const TabsContent = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Panel>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Panel>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.Panel ref={ref} className={cn('mt-2', className)} {...props} />
+));
+TabsContent.displayName = 'TabsContent';
+
+export { Tabs, TabsList, TabsTrigger, TabsContent };
